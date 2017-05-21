c3ca5225c6f599ac3b51ccf4a964df0ef3d9c816
tangfeixiong <tangfx128@gmail.com>
2017-05-21 15:37:27 +0200

    trainning

---
+++ java/https0x3A0x2F0x2Fdocs.oracle.com0x2Fjavase0x2Ftutorial/uiswing0x2Fexamples0x2Fevents0x2FKeyEventDemoProject/src/trainning/KeyEventDemo.java
@@ -0,0 +1,98 @@
+package trainning;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+/**
+ * Created by cloudadmin on 2017/5/21.
+ */
+public class KeyEventDemo  extends JFrame
+{
+    JTextArea displayArea;
+    JTextField typingArea;
+    DirectionKeyListener listener;
+
+    public static void main(String[] args) {
+        /* Use an appropriate Look and Feel */
+        try {
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsLookAndFeel");
+            //UIManager.setLookAndFeel("com.sun.java.swing.plaf.gtk.GTKLookAndFeel");
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+        } catch (UnsupportedLookAndFeelException ex) {
+            ex.printStackTrace();
+        } catch (IllegalAccessException ex) {
+            ex.printStackTrace();
+        } catch (InstantiationException ex) {
+            ex.printStackTrace();
+        } catch (ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        /* Turn off metal's use of bold fonts */
+        UIManager.put("swing.boldMetal", Boolean.FALSE);
+
+        //Schedule a job for event dispatch thread:
+        //creating and showing this application's GUI.
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+
+    /**
+     * Create the GUI and show it.  For thread safety,
+     * this method should be invoked from the
+     * event-dispatching thread.
+     */
+    private static void createAndShowGUI() {
+        //Create and set up the window.
+        KeyEventDemo frame = new KeyEventDemo("KeyEventDemo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        //Set up the content pane.
+        frame.addComponentsToPane();
+
+
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void addComponentsToPane() {
+        listener = new DirectionKeyListener();
+
+        JButton button = new JButton("Clear");
+        button.addActionListener(this.listener);
+
+        typingArea = new JTextField(20);
+        typingArea.addKeyListener(this.listener);
+        this.listener.setTypingArea(typingArea);
+
+        //Uncomment this if you wish to turn off focus
+        //traversal.  The focus subsystem consumes
+        //focus traversal keys, such as Tab and Shift Tab.
+        //If you uncomment the following line of code, this
+        //disables focus traversal and the Tab events will
+        //become available to the key event listener.
+        //typingArea.setFocusTraversalKeysEnabled(false);
+
+        displayArea = new JTextArea();
+        displayArea.setEditable(false);
+        JScrollPane scrollPane = new JScrollPane(displayArea);
+        scrollPane.setPreferredSize(new Dimension(375, 125));
+        this.listener.setDisplayArea(displayArea);
+
+        getContentPane().add(typingArea, BorderLayout.PAGE_START);
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        getContentPane().add(button, BorderLayout.PAGE_END);
+    }
+
+    public KeyEventDemo(String name) {
+        super(name);
+    }
+
+}
