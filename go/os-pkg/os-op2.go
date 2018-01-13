0c1cf90d05ed1359fbe3b94e41c001f2214dd7cf
tangfeixiong <tangfx128@gmail.com>
2018-01-12 22:26:05 -0800

    os exec with args

--- go/os-pkg/os-op2.go
+++ go/os-pkg/os-op2.go
@@ -12,4 +12,13 @@ func main() {
 		log.Fatal(err)
 	}
 	fmt.Printf("The date is %s\n", out)
+
+	cmd := exec.Command("ls")
+	cmd.Args = []string{"-l", "/"}
+	out, err = cmd.Output()
+	if err != nil {
+		fmt.Println("Error:", err.Error())
+	} else {
+		fmt.Println(string(out))
+	}
 }
